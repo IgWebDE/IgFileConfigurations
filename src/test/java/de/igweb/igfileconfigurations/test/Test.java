617627fdbe83a4d6c8e9930ffe87269332cf71c0
JonasDevDE <73913242+JonasDevDE@users.noreply.github.com>
2022-11-03 16:31:04 +0100

    General update

--- src/test/java/de/igweb/igfileconfigurations/test/Test.java
+++ src/test/java/de/igweb/igfileconfigurations/test/Test.java
@@ -1,33 +1,11 @@
 package de.igweb.igfileconfigurations.test;
 
-import lombok.SneakyThrows;
-
 public class Test {
 
-    @SneakyThrows
     public static void main(String[] args) {
-
-        // save a new configuration
-        TestConfiguration tc = TestConfiguration.builder()
-                .testA("TestA")
-                .testB("TestB")
-                .testC("TestC")
-                .testD("TestD")
-                .testE("TestE")
-                .build();
-        Thread.sleep(100);
-        tc.save();
-
         TestConfiguration testConfiguration = new TestConfiguration();
-       Thread.sleep(100);
-        testConfiguration.getValues().forEach((k, v) -> {
-            System.out.println(k + " = " + v);
-        });
-       Thread.sleep(100);
-        testConfiguration.setTestA("New TestA");
-       testConfiguration.setTestB("New TestB");
-
-       testConfiguration.save();
+        testConfiguration.setTestB("B");
+        testConfiguration.save();
     }
 
 }
