b8bba5b85411638cdf1f374dd5d88c8e075b7066
sakul-the-one <68478969+sakul-the-one@users.noreply.github.com>
2022-11-06 10:29:27 +0100

    Update FileSettings.java
    idk java, aber c#... habs bissen erweitert

--- src/main/java/de/igweb/igfileconfigurations/FileSettings.java
+++ src/main/java/de/igweb/igfileconfigurations/FileSettings.java
@@ -9,6 +9,34 @@ public @interface FileSettings {
     /**
      * The path to the file
      **/
+    public string Path;
+    public string value;
+    public string Passwort;
     String value();
+    
+    public string ReadFile(string Pass) 
+    {
+        if (Pass == Passwort) {
+            /*Read&Co*/ 
+        }
+        
+    }
+    public void WriteFile(string Pass) 
+    {
+        if (Pass == Passwort) {
+            /*Write&Co*/ 
+        }
+        
+    }
+    public void CreateNewFile(string path, string passwort)
+    { if(Path == null && Passwort == null) {
+    Path = path;
+    Passwort = passwort; }
+    }
+    public void ChangePasswort (string OldPasswort, string NewPasswort) 
+    {
+     if(OldPasswort == Passwort) Passwort = NewPasswort;   
+    }
 
 }
+
